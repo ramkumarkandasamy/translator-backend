b10974dfeff0c8cdd7820bd860e6f7aeaf08fc52
ramkumarkandasamy <123794214+ramkumarkandasamy@users.noreply.github.com>
2025-10-30 08:18:29 +0530

    server.js

--- server.js
+++ server.js
@@ -34,5 +34,7 @@ app.post("/translate", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
+
